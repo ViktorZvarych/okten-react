9fd86d9e554c300472e7591dad44365d9fd4adbd
Viktor Zvarych <viktor.zvarych@gmail.com>
2024-02-01 12:54:09 +0200

    initial master branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import Characters from "./Components/Characters";
-import './App.css';
 
 const App = () => {
     return (
-        <section>
-            <Characters/>
-        </section>
+        <>
+        </>
     );
 };
 

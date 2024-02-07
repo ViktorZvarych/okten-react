e4930a2bdf150f92576e0e54e66456b413dc33e7
Viktor Zvarych <viktor.zvarych@gmail.com>
2024-02-07 22:17:25 +0200

    My-notes/axios

--- src/components/AxiosComponent.js
+++ src/components/AxiosComponent.js
@@ -1,20 +1,20 @@
 // ---AXIOS---
-
+import React from 'react';
 import axios from "axios";
 
-const AxiosComponent = () => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(response => console.log('1. fetch', response));
+    const AxiosComponent = () => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(response => console.log('1. fetch', response));
 
-    axios('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.data)
-        .then(response => console.log('2. axios', response));
+        axios('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.data)
+            .then(response => console.log('2. axios', response));
 
-    axios
-        .create({baseURL: 'https://jsonplaceholder.typicode.com'})
-        .get('/posts')
-        .then(({data}) => console.log('3. axios with create baseURL', data));
+        axios
+            .create({baseURL: 'https://jsonplaceholder.typicode.com'})
+            .get('/posts')
+            .then(({data}) => console.log('3. axios with create baseURL', data));
 
 // src/
 // |---components/
@@ -33,59 +33,61 @@ const AxiosComponent = () => {
 
 
 // constants/urls
-    const postsJsonBaseUrl = 'https://jsonplaceholder.typicode.com'
-    const spaceXJsonBaseUrl = 'https://api.spacexdata.com/v3'
+        const postsJsonBaseUrl = 'https://jsonplaceholder.typicode.com'
+        const spaceXJsonBaseUrl = 'https://api.spacexdata.com/v3'
 
-    const users = '/users'
-    const posts = '/posts'
-    const launches = '/launches'
+        const users = '/users'
+        const posts = '/posts'
+        const launches = '/launches'
 
-    const urls = {
-        users: {
-            base:'/users',
-            byId: (id) => `${users}/${id}`
-        },
-        posts:{
-            base:posts,
-            byId: (id) => `${posts}/${id}`
-        },
-        launches:{
-            base:launches,
-            byId: (id) => `${launches}/${id}`
+        const urls = {
+            users: {
+                base:'/users',
+                byId: (id) => `${users}/${id}`
+            },
+            posts:{
+                base:posts,
+                byId: (id) => `${posts}/${id}`
+            },
+            launches:{
+                base:launches,
+                byId: (id) => `${launches}/${id}`
+            }
         }
-    }
 
-    // export {
-    // postsJsonBaseUrl,
-    // spaceXJsonBaseUrl,
-    // urls
-    // }
+        // export {
+        // postsJsonBaseUrl,
+        // spaceXJsonBaseUrl,
+        // urls
+        // }
 
 //--- services/axiosAPIService.js
-    // import axios from "axios";
-    // import {postsJsonBaseUrl} from "../constants/urls";
+        // import axios from "axios";
+        // import {postsJsonBaseUrl} from "../constants/urls";
 
-    const axiosAPIService = axios.create({baseURL: postsJsonBaseUrl})
+        const axiosAPIService = axios.create({baseURL: postsJsonBaseUrl})
 
-    // export {axiosAPIService}
+        // export {axiosAPIService}
 
 //--- services/postsService.js
-    // import {axiosAPIService} from "./axiosPostService.service";
-    // import {urls} from "../constants/urls";
+        // import {axiosAPIService} from "./axiosPostService.service";
+        // import {urls} from "../constants/urls";
 
-    const postsService = {
-        getAll: () => axiosAPIService.get(urls.posts.base)
-    }
+        const postsService = {
+            getAll: () => axiosAPIService.get(urls.posts.base)
+        }
 
-    // export {postsService}
+        // export {postsService}
 
-    postsService.getAll().then(({data}) => console.log('4. postService', data));
+        postsService.getAll().then(({data}) => console.log('4. postService', data));
 
-    return (
-        <div>
+        return (
+            <div>
 
-        </div>
-    );
-};
+            </div>
+        );
+    };
+
 
 export default AxiosComponent;
+

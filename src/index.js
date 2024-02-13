47fb4d98c2e1899b95c49387c5b32b4937b85f93
Viktor Zvarych <viktor.zvarych@gmail.com>
2024-02-13 12:31:45 +0200

    Lesson4/Router/part1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
 import './index.css';
-import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main/>
+    }
+])
+
 root.render(
-  <App />
+  <RouterProvider router={routes}/>
 );
 

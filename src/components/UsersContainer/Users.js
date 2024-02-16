8bb23edcc929f79ff7f97714f2aecee09861d603
Viktor Zvarych <viktor.zvarych@gmail.com>
2024-02-16 20:27:49 +0200

    Lesson5/Router-useParams-loader/task1-refactor

--- src/components/UsersContainer/Users.js
+++ src/components/UsersContainer/Users.js
@@ -1,13 +1,17 @@
 import React, {useEffect, useState} from 'react';
 
 import User from "./User";
+import {useNavigate} from "react-router-dom";
 
 const Users = ({data}) => {
+    const navigate = useNavigate();
+    navigate('', {state: {users: data}})
+
     const [users, setUsers] =useState([]);
 
     useEffect(() => {
         setUsers(data)
-    }, []);
+    }, [data]);
 
     return (
         <div>
